7767e01b5a65fc490c7c56d6dba2a3c48d07f797
BugHunter-X <askzenai@gmail.com>
2024-12-28 07:24:05 +0100

    Add IncorrectPropType.js file

---
+++ IncorrectPropType.js
@@ -0,0 +1,1 @@
+This error occurs when you are using a component that expects a specific prop type, but you are passing it a value of a different type. For example, if a component expects a string prop, but you are passing it a number, you will get this error.
